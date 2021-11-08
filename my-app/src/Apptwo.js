0eba536daf8a3540f473ffc9e5c2f108a72e5cff
Emre Dogan <emredoga@outlook.com>
2021-11-08 10:30:48 +0100

    Quick fix for routes error. Entered main App path.

--- my-app/src/Apptwo.js
+++ my-app/src/Apptwo.js
@@ -13,10 +13,10 @@ function Apptwo() {
         <BrowserRouter>
             <Routes>
                 {/** paths to different pages */}
-                <Route path="/journalist" element={}/>
-                <Route path="/editor" element={}/>
-                <Route path="/journalist/employees" element={}/>
-                <Route path="/editor" element={}/>
+                <Route path="/journalist" element={<App/>}/>
+                <Route path="/editor" element={<App/>}/>
+                <Route path="/journalist/employees" element={<App/>}/>
+                <Route path="/editor" element={<App/>}/>
                 
                 {/** "homepage" */}
                 <Route path="/" element={<App/>}/>
