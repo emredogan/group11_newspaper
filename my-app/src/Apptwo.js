ea4566551dbb3dbc6c1d97a2ceb67f45aef98cf8
Daniel Dencker <dsd139@gmail.com>
2021-11-08 10:19:07 +0100

    New Routes for real

--- my-app/src/Apptwo.js
+++ my-app/src/Apptwo.js
@@ -17,7 +17,14 @@ function Apptwo() {
                 <Route path="/editor" element={}/>
                 <Route path="/journalist/employees" element={}/>
                 <Route path="/editor" element={}/>
-                
+                <Route path="/journalist/task" element={}/>
+                <Route path="/journalist/task/create-task" element={}/>
+                <Route path="/journalist/task/create-task" element={}/>
+                <Route path="/journalist/article-ideas" element={}/>
+                <Route path="/journalist/article-ideas/create-idea" element={}/>
+                <Route path="/editor/employees" element={}/>
+                <Route path="/editor/article-ideas/create-idea" element={}/>
+                <Route path="/editor/newspaper" element={}/>
                 {/** "homepage" */}
                 <Route path="/" element={<App/>}/>
             </Routes>
