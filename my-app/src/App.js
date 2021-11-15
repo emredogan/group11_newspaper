4243ae22c2ec15b42a5914f9020868801b8476b0
Emre Dogan <emredoga@outlook.com>
2021-11-15 13:57:28 +0100

    added authentication

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,9 +6,28 @@ import 'typeface-roboto';
 import Navigation from './Components/Navigation';
 import Welcome from './Components/Welcome';
 import { BrowserRouter as Router, Route, Swicth } from 'react-router-dom';
+import { initializeParse } from '@parse/react';
+import Parse from 'parse';
+
+
 
 
 function App() {
+  process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
+
+  Parse.initialize(
+    "oHPQf2X0grpeXteCxywdMAvZ6BaV3lj1o3SUJ4pJ",
+   "wE0CWYnPVC86eVLQAcvOUm37BFhqW6ZBZTcdCL5B",
+    "7Tqwx4xkFQ0tZDsoQrsI2nxJdlZxXaSev96e5Dtw")
+
+  Parse.serverURL = "https://parseapi.back4app.com/"
+
+
+  console.log("STARTING PARSE")
+
+  console.log(Parse.applicationId)
+
   const adminUser = {
     email: "admin@admin.com",
     password: "admin123"
@@ -25,12 +44,49 @@ function App() {
       setUser({
         name: details.name,
         email: details.email
+
       });
+
+      const user = new Parse.User();
+user.set("username", details.email);
+user.set("password", details.password);
+user.set("email", details.email);
+
+try {
+   user.signUp();
+  // Hooray! Let them use the app now.
+} catch (error) {
+  // Show the error message somewhere and let the user try again.
+  alert("Error: " + error.code + " " + error.message);
+}
+
+     
+      //   try {
+      //     // Since the signUp method returns a Promise, we need to call it using await
+      //     const createdUser = Parse.User.signUp(details.email, details.password);
+      //     alert(
+      //       `Success! User ${createdUser.getUsername()} was successfully created!`
+      //     );
+      //     return true;
+      //   } catch (error) {
+      //     // signUp can fail if any parameter is blank or failed an uniqueness check on the server
+      //     alert(`Error! ${error}`);
+      //     return false;
+        
+      // };
+
+
+
+
+      
+
     } else { 
       console.log("Details do not match");
       setError("Details do not match");
     }
   }
+
+  
 
 
   // the function that is called when we are trying to Logout
