626a135948d40af38723d734eb81220884b942e7
ETALATE <84042296+ETALATE@users.noreply.github.com>
2021-11-23 12:54:49 +0100

    small notes!

--- my-app/src/Components/JournalistCards.js
+++ my-app/src/Components/JournalistCards.js
@@ -10,8 +10,9 @@ function Cards() {
          <div className="welcome">
            <Header name={'Journalist'}/>
            <ul className='Cards'>
-            <li><Link to=""><MyButton text="Newspaper" imageURL="/newspaper.svg"></MyButton></Link></li>
-            <li><Link to=""><MyButton text="Article Idea" imageURL="/dialogue.svg"></MyButton></Link></li>
+               {/** if possible make Link reusable */}
+            <li><Link to="" style={{ textDecoration: 'none'}}><MyButton text="Newspaper" imageURL="/newspaper.svg"></MyButton></Link></li>
+            <li><Link to="" style={{ textDecoration: 'none'}}><MyButton text="Article Idea" imageURL="/dialogue.svg"></MyButton></Link></li>
             <li><Link to="/journalist/task" style={{ textDecoration: 'none'}}><MyButton text="Overview of Tasks" imageURL="/statistic.svg"></MyButton></Link></li>
            </ul>
         </div>
