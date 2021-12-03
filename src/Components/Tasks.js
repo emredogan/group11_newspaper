8160227bed91537ccc2cf32468f64b0fb4af556d
ETALATE <84042296+ETALATE@users.noreply.github.com>
2021-12-03 15:13:17 +0100

    one task is added to table

--- src/Components/Tasks.js
+++ src/Components/Tasks.js
@@ -1,49 +1,15 @@
 import { Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import Parse from "parse";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Tasks() {
-  {
-    /** just testing - should be db data */
-  }
   const [tasks, setTasks] = useState([]);
 
   console.log("STARTS GETTING THE OBJECT");
 
   const task = Parse.Object.extend("Task");
   const query = new Parse.Query(task);
-  query.get("Ws4qPsemkG").then(
-    (task) => {
-      // The object was retrieved successfully.
-      console.log(task);
-      const status = task.get("status");
-      const title = task.get("title");
-      const description = task.get("description");
-      const section = task.get("section");
-      const responsible = task.get("responsible");
-      const deadline = task.get("deadline");
-
-      console.log(status);
-
-      setTasks({
-        status: status,
-        title: title,
-        des: description,
-        section: section,
-        resp: responsible,
-        deadl: deadline,
-      });
-      console.log("Tasks object");
-
-      console.log(tasks);
-    },
-    (error) => {
-      console.log(error);
-      // The object was not retrieved successfully.
-      // error is a Parse.Error with an error code and message.
-    }
-  );
 
   const renderTask = (task, index) => {
     return (
@@ -57,6 +23,42 @@ function Tasks() {
       </tr>
     );
   };
+
+  useEffect(() => {
+    //code
+    query.get("Ws4qPsemkG").then(
+      (task) => {
+        // The object was retrieved successfully.
+        console.log(task);
+        const status = task.get("status");
+        const title = task.get("title");
+        const description = task.get("description");
+        const section = task.get("section");
+        const responsible = task.get("responsible");
+        const deadline = task.get("deadline");
+  
+        console.log(status);
+  
+        setTasks({
+          status: status,
+          title: title,
+          des: description,
+          section: section,
+          resp: responsible,
+          deadl: deadline,
+        });
+        console.log("Tasks object");
+  
+        console.log(tasks);
+      },
+      (error) => {
+        console.log(error);
+        // The object was not retrieved successfully.
+        // error is a Parse.Error with an error code and message.
+      }
+    );
+    console.log('render stuff')
+  },[])
 
   return (
     <div className="tasksContainer">
