4dafd2901733bedc8a311554937b975f799f0b7c
Gina-Josephine <38035820+gina-josephine@users.noreply.github.com>
2021-12-04 17:14:55 +0100

    account page is now fetching email from database and lets you register sick leave

--- src/Components/Account.js
+++ src/Components/Account.js
@@ -1,37 +1,54 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Form } from "react-bootstrap";
 import Parse from "parse";
 
-const Notifications = () => {
-  
-  const getCurrentUser = async function () {
-    const currentUser = await Parse.User.currentAsync();
-    if (currentUser !== null) {
-      alert(
-        `Being directed to your account page: ${currentUser.get('username')}`,
-      );
-    }
-    return currentUser;
-  };
-  
-  try {
-    getCurrentUser()
-  } catch (error) {
-    alert("Error: " + error.code + " " + error.message);
-  }
-    
-  return (
-    <ul>
-      <li>
-        <header className="welcome">
-          <h2>
-            Welcome journalist this is your account. 
-          </h2>
-        </header>
-      </li>
-    </ul>
+const Account = () => {
+  const [username, setUsername] = useState("");
+  const [date, setDate] = useState();
 
+  useEffect(() => {
+    async function getCurrentUser() {
+      if (username === "") {
+        const currentUser = await Parse.User.currentAsync();
+        if (currentUser !== null) {
+          setUsername(currentUser.getUsername());
+        }
+        const sick = Parse.Object.extend("sick");
+        const newSickLeave = new sick();
+        newSickLeave.set("date", date);
+      }
+    }
+    getCurrentUser();
+  }, [username, date]);
+
+  return (
+    <>
+ 
+    <form className="loginForm">
+      <div className="form-inner">
+      <header className="welc">
+        <h2> Hi &nbsp;<span> {username} </span> , &nbsp;this is your account. </h2>
+        </header>
+       
+          <Form.Group className="formpart" controlId="formDate">
+            <Form.Label>Register sick leave:</Form.Label>
+            <Form.Control
+              type="date"
+              onChange={(e) => setDate(e.target.value)}
+            />
+          </Form.Group>
+        
+
+        {/* <input className="thebutton" type="submit" value="Update"></input>
+        <input
+          className="thebutton"
+          type="submit"
+          value="Delete Account">
+        </input> */}
+      </div>
+    </form>
+    </>
   );
 };
 
-  export default Notifications;
-  
+export default Account;
--- src/Components/Notifications.js
+++ src/Components/Notifications.js
@@ -1,15 +1,12 @@
 import React from "react";
 const Notifications = () => {
     return (
-      <ul>
-        <li>
+     
           <header className="welcome">
             <h2>
               There are no new notifications, you can relax!
             </h2>
           </header>
-        </li>
-      </ul>
     );
   };
   
