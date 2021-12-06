a4eae98bf008bbc84818203cfc1b64a7cb8045e8
ETALATE <84042296+ETALATE@users.noreply.github.com>
2021-12-07 00:23:41 +0100

    discard div

--- src/Components/Tasks.js
+++ src/Components/Tasks.js
@@ -6,9 +6,7 @@ function Tasks() {
   return (
     <>
       <div className="screenContain">
-        <div className="topContainer">
         <BtnComponent path={"/journalist/task/create-task"} text="NEW TASK" />
-        </div>
         <header className="screentitle">
           <h2>Overview of Tasks</h2>
         </header>
