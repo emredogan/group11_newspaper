3c2b0efdfb6762372f9b5757cdfd7615d2eb75a5
ETALATE <84042296+ETALATE@users.noreply.github.com>
2021-12-09 19:16:06 +0100

    New form

--- src/Components/FormComponents/DeadlineForm.js
+++ src/Components/FormComponents/DeadlineForm.js
@@ -7,7 +7,7 @@ export default function DeadlineForm(props) {
   return (
     <Form.Group className="formpart" controlId="formDate">
       <Form.Label>Deadline</Form.Label>
-      <Form.Control type="date"  onChange={(e) => props.setDate(e.target.value)} />
+      <Form.Control type="date"   onChange={(e) => props.setDate(e.target.value)} />
     </Form.Group>
   );
 }
