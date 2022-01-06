3d5e9157461cb170d91a48297f65a466a8b9ade1
Gina-Josephine <38035820+gina-josephine@users.noreply.github.com>
2022-01-06 20:57:21 +0100

    changed table for the daily task load, so it filters the current date

--- src/Components/DailyTasks.js
+++ src/Components/DailyTasks.js
@@ -1,26 +1,25 @@
 import React from "react";
 import Breadcrumb from "react-bootstrap/Breadcrumb";
-import Progressbar from './FormComponents/Progress_bar';
-import OverviewTable from "./OverviewTable";
-
+import Progressbar from "./FormComponents/Progress_bar";
+import DailyOverviewTable from "./DailyOverviewTable";
 
 const DailyTasks = () => {
   return (
-  <>
-    <div> 
-      <Breadcrumb>
-        <Breadcrumb.Item href="/">Home</Breadcrumb.Item>
-        <Breadcrumb.Item active>Daily Task Load</Breadcrumb.Item>
-      </Breadcrumb>
-      <div className="screenContain">
-      <header className="screentitle">
-          <h2>Daily Task Load</h2>
-        </header>
-        <Progressbar margin="0" bgcolor="#387B19" progress='25' height={25} />
-        <OverviewTable objectName="Task" />
+    <>
+      <div>
+        <Breadcrumb>
+          <Breadcrumb.Item href="/">Home</Breadcrumb.Item>
+          <Breadcrumb.Item active>Daily Task Load</Breadcrumb.Item>
+        </Breadcrumb>
+        <div className="screenContain">
+          <header className="screentitle">
+            <h2>Daily Task Load</h2>
+          </header>
+          <Progressbar margin="0" bgcolor="#387B19" progress="25" height={25} />
+          <DailyOverviewTable objectName="Task" />
         </div>
-    </div>
-  </>
+      </div>
+    </>
   );
 };
 
