17eed3b7c8066590035db61047bb9966bfeff58a
Gina-Josephine <38035820+gina-josephine@users.noreply.github.com>
2022-01-06 12:50:40 +0100

    Daily Task Load Screen has the progress bar

--- src/Components/DailyTasks.js
+++ src/Components/DailyTasks.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Breadcrumb from "react-bootstrap/Breadcrumb";
+import Progressbar from './FormComponents/Progress_bar';
 
 const DailyTasks = () => {
   return (
@@ -7,12 +8,15 @@ const DailyTasks = () => {
  <>
       <Breadcrumb>
         <Breadcrumb.Item href="/">Home</Breadcrumb.Item>
-        <Breadcrumb.Item active>Notifications</Breadcrumb.Item>
+        <Breadcrumb.Item active>Daily Task Load</Breadcrumb.Item>
       </Breadcrumb>
       </>
-      <header className="welco">
-      <h2>Daily Task Load</h2>
-    </header>
+      <div className="screenContain">
+      <header className="screentitle">
+          <h2>Daily Task Load</h2>
+        </header>
+        <Progressbar bgcolor="#387B19" progress='1/4'  height={25} />
+        </div>
     </div>
   );
 };
