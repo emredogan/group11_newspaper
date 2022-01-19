d8902cdbd4e3016a997189d8b425697d43614360
ETALATE <84042296+ETALATE@users.noreply.github.com>
2022-01-19 15:13:43 +0100

    Tanslation style finish

--- src/Components/Translation.js
+++ src/Components/Translation.js
@@ -58,7 +58,7 @@ export default function Translation() {
           className="transButton"
           onClick={automaticTranslation}
         >
-          ?
+          Go!
         </Button>
       </div>
     </>
